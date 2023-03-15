51672199fa320a9602b1ffec8a9256f2a871bdaf
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-03-15 18:31:01 -0500

    mm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 4000;
+const port = 80;
 //const dotenv = require('dotenv');
 require('dotenv').config();
 
