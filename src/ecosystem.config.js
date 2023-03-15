3fa8155a7bc8dcbd5d0a15a303f8dd592cdfc0e9
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-03-14 22:15:02 -0500

    update serv

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -2,23 +2,31 @@ module.export = {
   apps: [
     {
       name: 'my-app',
-      script: 'index.js',
+      script: '/var/www/api/api-test/src/index.js',
       env: {
-        POBOX_ZIPCODE: '33172-2779',
-        CUSTOMER_NAME: 'Tracking PTY',
-        CUSTOMER_WEBSITE: 'trackingpanama.com',
-        MAIL_FROM: 'trackingpty@trackingpanama.com',
-        MAIL_TO_ADMIN: 'casilleros@trackingpanama.com',
-        POBOX_ADDRESS: '1345 NW 98TH CT, ST2 SP',
-        POBOX_COUNTRY: 'Estados Unidos',
-        POBOX_PHONE: '(786) 360-2816',
-        POBOX_STATE:' Miami, Florida',
-        SERVER_MAIL: 'in-v3.mailjet.com',
-        SERVER_MAIL_AUTH_PWD: '7b320ca51fc9c26e27afeefcc61afd62',
-        SERVER_MAIL_AUTH_USER: 'ced8911a15a18484fce5d3aaff9be923',
-        SERVER_MAIL_PORT: '465',
-        SERVER_MAIL_secure: 'true'
-      }
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
     }
-  ]
+  ],
+  deploy : {
+    production : {
+      POBOX_ZIPCODE: '33172-2779',
+      CUSTOMER_NAME: 'Tracking PTY',
+      CUSTOMER_WEBSITE: 'trackingpanama.com',
+      MAIL_FROM: 'trackingpty@trackingpanama.com',
+      MAIL_TO_ADMIN: 'casilleros@trackingpanama.com',
+      POBOX_ADDRESS: '1345 NW 98TH CT, ST2 SP',
+      POBOX_COUNTRY: 'Estados Unidos',
+      POBOX_PHONE: '(786) 360-2816',
+      POBOX_STATE:' Miami, Florida',
+      SERVER_MAIL: 'in-v3.mailjet.com',
+      SERVER_MAIL_AUTH_PWD: '7b320ca51fc9c26e27afeefcc61afd62',
+      SERVER_MAIL_AUTH_USER: 'ced8911a15a18484fce5d3aaff9be923',
+      SERVER_MAIL_PORT: '465',
+      SERVER_MAIL_secure: 'true'
+    }
+  }
 }
